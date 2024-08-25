653ce3114b4a08f55396b046f5befff94572c018
AbdelrahmanEhab <abdoehab5008@gmail.com>
2024-08-26 00:12:13 +0300

    updates

--- Leetcode-Ques/3-Easy-LengthofLastWord.js
+++ Leetcode-Ques/3-Easy-LengthofLastWord.js
@@ -1,15 +1,17 @@
 // ! https://leetcode.com/problems/length-of-last-word/description/
-var lengthOfLastWord = function (s) {
-  let stringTrimAndSplitted = s.trim().split(" ");
-  console.log("stringTrimAndSplitted *****");
-  console.log(stringTrimAndSplitted);
-  let lastWordLength =
-    stringTrimAndSplitted[stringTrimAndSplitted.length - 1].length;
-  return lastWordLength;
-};
-let testCase = lengthOfLastWord("   fly me   to   the moon  ");
+// var lengthOfLastWord = function (s) {
+//   let stringTrimAndSplitted = s.trim().split(" ");
+//   console.log("stringTrimAndSplitted *****");
+//   console.log(stringTrimAndSplitted);
+//   let lastWordLength =
+//     stringTrimAndSplitted[stringTrimAndSplitted.length - 1].length;
+//   return lastWordLength;
+// };
+// let testCase = lengthOfLastWord("   fly me   to   the moon  ");
+let nums = [1, 2, 3, 4, 5];
+const leftProducts = new Array(nums.length).fill(1);
 console.log(" -----------------------------");
 console.log(" ----------------------");
-console.log(testCase);
+console.log(leftProducts);
 console.log(" ----------------------");
 console.log(" ------------------");
--- NeetCode-75Quest/4-productOfEachELementExcept.js
+++ NeetCode-75Quest/4-productOfEachELementExcept.js
@@ -1,18 +1,17 @@
 // ! 238. Product of Array Except Self
 // ? https://leetcode.com/problems/product-of-array-except-self/description/
 var productExceptSelf = function (nums) {
-  const n = nums.length;
-  const result = new Array(n);
+  const result = new Array(nums.length).fill(1);
 
   // Calculate prefix products
   let prefixProduct = 1;
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < nums.length; i++) {
     result[i] = prefixProduct;
     prefixProduct *= nums[i];
   }
   // Calculate suffix products and multiply with prefix products
   let suffixProduct = 1;
-  for (let i = n - 1; i >= 0; i--) {
+  for (let i = nums.length - 1; i >= 0; i--) {
     result[i] *= suffixProduct;
     suffixProduct *= nums[i];
   }
